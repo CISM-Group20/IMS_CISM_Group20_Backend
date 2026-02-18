37436bb15a1dd1e4447da339cbbfcada986bc506
Sanugi <divigalpitiyawdwmsg.21@uom.lk>
2026-02-18 13:47:38 +0530

    Input validation Policy

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,23 @@
 const  mongoose  = require("mongoose");
 var mongoURL = process.env.DBUrl;
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const connectDB = async() => {
         try {
            mongoose.set("strictQuery", false);
+           // Sanitize mongoURL
+           mongoURL = mongoSanitize.sanitize(mongoURL);
             await mongoose.connect(mongoURL);
             console.log('mongodb connection SUCCESS');
         } catch (error) {
             console.log('mongodb connection FAIL');
-            console.log(error);
+            // Controlled error response
+            if (error instanceof mongoSanitize.MongoSanitizeError) {
+                console.log('Suspicious input detected in DB connection string.');
+            } else {
+                console.log('Database connection error.');
+            }
             process.exit(1);
         }
    
